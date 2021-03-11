9d61cf963f213a0cf38cc42edfe76f922304bab3
peterpaw <pb.pawelczyk@gmail.com>
2021-03-11 10:26:08 +0100

    add navbar

--- app.js
+++ app.js
@@ -11,3 +11,35 @@ document.querySelector('#hero-btn').addEventListener('click', () => {
     behavior: 'smooth'
   })
 })
+
+// menu links
+document.querySelector('#link-activities').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector('#activities').scrollIntoView({
+    behavior: 'smooth'
+  })
+})
+document.querySelector('#link-about-us').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector('#about-us').scrollIntoView({
+    behavior: 'smooth'
+  })
+})
+document.querySelector('#link-contact').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector('#contact').scrollIntoView({
+    behavior: 'smooth'
+  })
+})
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('navbar').style.top = '0'
+  } else {
+    document.getElementById('navbar').style.top = '-70px'
+  }
+}
