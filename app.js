8e059280e27e806118567e05aea12405a7246b48
peterpaw <pb.pawelczyk@gmail.com>
2021-03-11 15:24:32 +0100

    add impressum and privacy policy

--- app.js
+++ app.js
@@ -60,11 +60,3 @@ document.querySelector('#link-contact').addEventListener('click', (e) => {
 window.onscroll = function () {
   scrollFunction()
 }
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('navbar').style.top = '0'
-  } else {
-    document.getElementById('navbar').style.top = '-70px'
-  }
-}
