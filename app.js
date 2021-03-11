93d6f85b257bd761ed064d75f42aa82f3d1b51a2
peterpaw <pb.pawelczyk@gmail.com>
2021-03-11 14:51:34 +0100

    add respnsive nav menu

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+const menu = document.querySelector('.links')
+const menuBtn = document.querySelector('.menu-btn')
+const cancelBtn = document.querySelector('.cancel-btn')
+const menuLinks = document.querySelectorAll('.menu-link')
+const logoWrapper = document.querySelector('.logo-wrapper')
+
+// mobile menu
+menuBtn.onclick = () => {
+  menu.classList.add('active')
+}
+cancelBtn.onclick = () => {
+  menu.classList.remove('active')
+}
+
+// close mobile menu on link click
+logoWrapper.onclick = () => {
+  menu.classList.remove('active')
+}
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active')
+  })
+})
+
+// scroll to top
 document.querySelector('#scrollToTop').addEventListener('click', () => {
   window.scrollTo({
     top: 0,
