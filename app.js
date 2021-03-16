0a6b57491410f11492dc0e0fbc8653ba656aee2e
peterpaw <pb.pawelczyk@gmail.com>
2021-03-16 11:39:34 +0100

    fix eventlisteners for secondary pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ menuBtn.onclick = () => {
   menu.classList.add('active')
   menuBtn.classList.add('hide')
 }
+
 cancelBtn.onclick = () => {
   menu.classList.remove('active')
   menuBtn.classList.remove('hide')
@@ -17,7 +18,9 @@ cancelBtn.onclick = () => {
 // close mobile menu on link click
 logoWrapper.onclick = () => {
   menu.classList.remove('active')
+  menuBtn.classList.remove('hide')
 }
+
 menuLinks.forEach((link) => {
   link.addEventListener('click', () => {
     menu.classList.remove('active')
@@ -34,33 +37,30 @@ document.querySelector('#scrollToTop').addEventListener('click', () => {
   })
 })
 
-if (document.querySelector('#hero-btn')) {
-  document.querySelector('#hero-btn').addEventListener('click', () => {
-    document.querySelector('#activities').scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
-}
+// document.querySelector('#hero-btn').addEventListener('click', () => {
+//   document.querySelector('#activities').scrollIntoView({
+//     behavior: 'smooth'
+//   })
+// })
 
-// menu links
-if (document.querySelector('#link-activities').length) {
-  document.querySelector('#link-activities').addEventListener('click', (e) => {
-    e.preventDefault()
-    document.querySelector('#activities').scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
+// // menu links
+// document.querySelector('#link-activities').addEventListener('click', (e) => {
+//   e.preventDefault()
+//   document.querySelector('#activities').scrollIntoView({
+//     behavior: 'smooth'
+//   })
+// })
 
-  document.querySelector('#link-about-us').addEventListener('click', (e) => {
-    e.preventDefault()
-    document.querySelector('#about-us').scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
-  document.querySelector('#link-contact').addEventListener('click', (e) => {
-    e.preventDefault()
-    document.querySelector('#contact').scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
-}
+// document.querySelector('#link-about-us').addEventListener('click', (e) => {
+//   e.preventDefault()
+//   document.querySelector('#about-us').scrollIntoView({
+//     behavior: 'smooth'
+//   })
+// })
+
+// document.querySelector('#link-contact').addEventListener('click', (e) => {
+//   e.preventDefault()
+//   document.querySelector('#contact').scrollIntoView({
+//     behavior: 'smooth'
+//   })
+// })
